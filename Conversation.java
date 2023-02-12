8b398fa04e2db778216d8b47fcb74158d5771c69
CindySun0309 <csun76@smith.edu>
2023-02-12 15:57:53 -0500

    I do not know what to print after the for loop

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,43 @@
+import java.util.Scanner;
+
 class Conversation {
 
   public static void main(String[] arguments) {
     // You will start the conversation here.
+    System.out.print("How many rounds?");
+
+    Scanner userInput = new Scanner(System.in);
+    int round = userInput.nextInt();
+
+    System.out.println("Hi there!  What's on your mind?");
+
+    for (int i = 1; i <= round; i++) {
+      Scanner userInput2 = new Scanner(System.in);
+      String text = userInput2.nextLine();
+      String words[] = text.split(" ");
+      for (int j = 0; j < words.length; j++){
+        if (words[j] == "I") {
+          words[j] = "you";
+        }
+        else if (words[j] == "me") {
+          words[j] = "you";
+        }
+        else if (words[j] == "am") {
+          words[j] = "are";
+        }
+        else if (words[j] == "you") {
+          words[j] = "I";
+        }
+        else if (words[j] == "my") {
+          words[j] = "your";
+        }
+        else if (words[j] == "your") {
+          words[j] = "my";
+        }
+        System.out.print(text);
+      }
+      
+    }
+    System.out.println("Mmm-hm.");
   }
 }
