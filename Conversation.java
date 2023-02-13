249f2d64145ad653f358ced790bcff1040303be9
CindySun0309 <csun76@smith.edu>
2023-02-12 19:26:02 -0500

    I have done the first part (which is auto response)

--- Conversation.java
+++ Conversation.java
@@ -11,11 +11,14 @@ class Conversation {
 
     System.out.println("Hi there!  What's on your mind?");
 
-    for (int i = 1; i <= round; i++) {
+    for (int i = 0; i < round; i++) {
       Scanner userInput2 = new Scanner(System.in);
       String text = userInput2.nextLine();
+      System.out.println("Mmm-hm.");
+
+
       String words[] = text.split(" ");
-      for (int j = 0; j < words.length; j++){
+      /*for (int j = 0; j < words.length; j++){
         if (words[j] == "I") {
           words[j] = "you";
         }
@@ -33,11 +36,10 @@ class Conversation {
         }
         else if (words[j] == "your") {
           words[j] = "my";
-        }
+        } 
         System.out.print(text);
-      }
+      }*/
       
     }
-    System.out.println("Mmm-hm.");
   }
 }
